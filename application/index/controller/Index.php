e5878ef32a113d2528bce061438d0eb1667bc9c2
zhangyiwen <zhang92107@sina.com>
2018-01-14 11:57:46 +0800

    导航通过控制器调active

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,13 +9,16 @@ class Index extends Controller
     public function index(){
 //        $data = Db::name('data')->find();
 //        $this->assign('result', $data);
+        $this->assign("appoint","index");
         return $this->fetch();
     }
     public function resume($name='thinkphp'){
         $this->assign('name',$name);
+        $this->assign("appoint","resume");
         return $this->fetch();
     }
     public function mycase(){
+        $this->assign("appoint","mycase");
         return $this->fetch();
     }
     public function test(){
