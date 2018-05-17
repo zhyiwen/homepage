b89eed6142502003da070e5a4a6bb3fb6a4a1d21
zhyiwen <zhang92107@sina.com>
2018-05-17 10:28:22 +0800

    添加抽奖功能

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,6 +1,6 @@
 <?php
 namespace app\admin\controller;
-
+use
 class Index
 {
     public function index()
---
+++ application/choujiang/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\choujiang\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
